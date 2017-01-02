a25e26a7297629424a2436077f6823c0ef7ec823
blizzak1234 <botnikov@gmail.com>
2017-01-02 16:38:16 +0700

    mantis1

---
+++ conftest.py
@@ -0,0 +1,69 @@
+# данный фаил позволяет использовать фикстуру во всех вспомогательных методах
+import pytest
+from fixture.application import Application
+import json
+import os.path
+#from fixture.db import DbFixture
+import importlib
+#import jsonpickle
+
+
+
+fixture = None
+target = None
+
+
+def load_config(file):
+    global target
+    if target is None:
+        # определяем путь конф.файла (target.json) относильного директории проекта
+        config_file = os.path.join(os.path.dirname(os.path.abspath(__file__)), file) #путь к файлу
+        with open(config_file) as f:
+            target = json.load(f)
+    return target
+
+
+# функция инициализирующая фикстуру
+@pytest.fixture
+def app(request):
+    global fixture
+    browser = request.config.getoption("--browser")
+    web_config = load_config(request.config.getoption("--target"))['web']
+    if fixture is None or not fixture.is_valid():
+        fixture = Application(browser=browser, base_url=web_config['baseUrl'])
+    return fixture
+
+
+
+@pytest.fixture(scope="session", autouse=True)
+def stop(request):
+    def fin():
+        fixture.session.ensure_logout()
+        fixture.destroy()
+    request.addfinalizer(fin)
+    return fixture
+
+
+def pytest_addoption(parser):
+    parser.addoption("--browser", action="store", default="firefox")
+    parser.addoption("--target", action="store", default="target.json")
+
+
+# функция, помогающая питону определять какой фаил с тестовыми данными использовать. пробегает по именам и определять какой фаил брать
+def pytest_generate_tests(metafunc):
+    for fixture in metafunc.fixturenames:
+        if fixture.startswith("data_"):
+            testdata = load_from_module(fixture[5:])
+            metafunc.parametrize(fixture, testdata, ids=[str(x) for x in testdata])
+        elif fixture.startswith("json_"):
+            testdata = load_from_json(fixture[5:])
+            metafunc.parametrize(fixture, testdata, ids=[str(x) for x in testdata])
+
+
+def load_from_module(module):
+    return importlib.import_module("data.%s" % module).testdata
+
+def load_from_json(file):
+    # путь к файлу
+    with open(os.path.join(os.path.dirname(os.path.abspath(__file__)), "data/%s.json" % file)) as f:
+        return jsonpickle.decode(f.read())
---
+++ fixture/application.py
@@ -0,0 +1,38 @@
+from selenium import webdriver
+from selenium.webdriver.firefox.firefox_binary import FirefoxBinary
+from fixture.session import SessionHelper
+
+
+class Application:
+    def __init__(self, browser, base_url):
+        #self.wd = WebDriver(firefox_binary=FirefoxBinary("C:\\Program Files\\FF_ESR\\firefox.exe"))
+        if browser == "firefox":
+            self.wd = webdriver.Firefox()
+        elif browser == "chrome":
+            self.wd = webdriver.Chrome()
+        elif browser == "ie":
+            self.wd = webdriver.Ie()
+        else:
+            raise ValueError("Unrecognized browser %s" % browser)
+        self.wd.implicitly_wait(1)
+        self.session = SessionHelper(self)
+
+
+
+    def destroy(self):
+        self.wd.quit()
+
+
+    def is_valid(self):
+        try:
+            self.wd.current_url
+            return True
+        except:
+            return False
+
+    def change_field_value(self, field_name, text):
+        wd = self.wd
+        if text is not None:
+            wd.find_element_by_name(field_name).click()
+            wd.find_element_by_name(field_name).clear()
+            wd.find_element_by_name(field_name).send_keys(text)
